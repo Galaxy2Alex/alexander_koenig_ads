8f11c9aed6d0dae3e46f969327e02b20e5936de3
Odysseius <galaxy2alex@gmail.com>
2019-03-27 12:17:15 +0000

    Code done.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
 	printf("Welcome to Tic Tac Toe (Naughts and Crosses)\n\n");
 	
 	char gamesave[9] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
-
+	short int boolPlayer = 0;
 	char symbols[2] = {'O', 'X'};
 
 	printf("   %c|%c|%c\n", gamesave[0], gamesave[1], gamesave[2]);
@@ -16,12 +16,10 @@ int main() {
 	printf("   %c|%c|%c\n", gamesave[6], gamesave[7], gamesave[8]);
 	
 	do {
-		printf("Player X, Enter a Letter, corresponding to the space you want to fill\n");
+		printf("Player %hd, Enter a Letter, corresponding to the space you want to fill. Entering the wrong character will forfeit your turn.\n", boolPlayer + 1);
 		char input;
-		scanf("%c", &input);
+		scanf(" %c", &input);
 
-		char boolPlayer = 0;
-		
 		if(input == 65 || input == 97 && gamesave[0] == 65) {
 			gamesave[0] = symbols[boolPlayer];
 		} else if((input == 66 || input == 98) && gamesave[1] == 66) {
@@ -41,13 +39,44 @@ int main() {
 		} else if((input == 73 || input == 105) && gamesave[8] == 73) {
 			gamesave[8] = symbols[boolPlayer];
 		}
-		
+	
 		printf("");
 		printf("   %c|%c|%c\n", gamesave[0], gamesave[1], gamesave[2]);
 		printf("   -|-|-\n");
 		printf("   %c|%c|%c\n", gamesave[3], gamesave[4], gamesave[5]);
 		printf("   -|-|-\n");
 		printf("   %c|%c|%c\n", gamesave[6], gamesave[7], gamesave[8]);
+		
+		if(gamesave[0] == gamesave[1] && gamesave[1] == gamesave[2]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[3] == gamesave[4] && gamesave[4] == gamesave[5]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[6] == gamesave[7] && gamesave[7] == gamesave[8]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[0] == gamesave[3] && gamesave[3] == gamesave[6]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[1] == gamesave[4] && gamesave[4] == gamesave[7]) {
+			printf("Player %hd has won!", boolPlayer + 1);	
+			break;
+		} else if(gamesave[2] == gamesave[5] && gamesave[5] == gamesave[8]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[0] == gamesave[4] && gamesave[4] == gamesave[8]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[2] == gamesave[4] && gamesave[4] == gamesave[6]) {
+			printf("Player %hd has won!", boolPlayer + 1);
+			break;
+		} else if(gamesave[0] != 65 && gamesave[1] != 66 && gamesave[2] != 67 &&
+			gamesave[3] != 68 && gamesave[4] != '5' && gamesave[5] != '6' &&
+			gamesave[6] != '7' && gamesave[7] != '8' && gamesave[8] != '9') {
+			printf("Tie!");
+			break;
+		}
 		
 		if (boolPlayer == 0) {
 			boolPlayer = 1;
